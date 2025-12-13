1c9427512b6c860a0281816a9fc4ce9cf36e92a2
marwanf8 <marwanfathy86@gmail.com>
2025-12-13 14:41:26 +0200

    Update Featured Section

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const HotelCard = ({room, index}) => {
     return(
-     <Link to={'/rooms/' + romm._id} onClick={() => scrollTo(0,0)} key={room._id}>
-        <img src={romm.images[0]} alt="" className="reative max-w-70 w-full rounded-xl overflow-hidden
-        bg-white text-gray-500/90 shadow-[0px_4px_4px-rgba(0,0,0,0.5)]"/>
-        {index % 2 === 0 &&<p className="px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-meduim rounded-full">
-            Best Seller</p>}
+     <Link to={'/rooms/' + room._id} onClick={() => scrollTo(0,0)} key={room._id} className="reative max-w-70 w-full rounded-xl overflow-hidden
+        bg-white text-gray-500/90 shadow-[0px_4px_4px-rgba(0,0,0,0.5)]">
+        <img src={room.images[0]} alt="" />
+        
+        {index % 2 === 0 &&<p className="px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-meduim
+         rounded-full">Best Seller</p>}
+
         <div className="p-4 pt-5">
             <div className="flex items-center jusitify-between">
                 <p className="font-playfair text-hl font-meduim text-gray-800">{room.hotel.name}</p>
                 <div className="flex items-center gap-1">
                     <img src={assets.starIconFilled} alt="star-icon" /> 4.5
                 </div>
+            </div>
+            <div className="flex items-center gap-1 text-sm">
+                <img src={assets.locationIcon} alt="location-icon" />
+                <span>{room.hotel.address}</span>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+                <p><span className="text-xl text-gray-800">${room.pricePerNight}</span>/night</p>
+                <button className="px4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50
+                transition-all cursor-pointer">Book Now</button>
             </div>
         </div>
 
