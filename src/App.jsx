b8b9ceec340202e9c3ca6080829d2b8a9d7427b6
marwanf8 <marwanfathy86@gmail.com>
2025-12-14 14:57:30 +0200

    Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import {Route, Routes, useLocation} from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const isOwnerPath = useLocation().pathname.includes("owner");
         <Route path='/' element= {<Home/>}/>
       </Routes>
     </div>
+    <Footer />
     </div>
   )
 }
